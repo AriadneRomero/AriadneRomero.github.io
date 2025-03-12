30a485fcef0125940c2265491e477e30540de434
Ariadne Romero <88512668+AriadneRomero@users.noreply.github.com>
2025-03-11 19:40:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #5B8FB9;">Sumergiéndome en SQL</span>')
+  .typeString('<span style="color: #5B8FB9;">Sumergiéndome en Bases de datos y aprendizaje automático</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
